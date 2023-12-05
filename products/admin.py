caa5ec995b9e0d5da90264b8c794ec0617b9c080
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-12-06 00:27:14 +0200

    add (product,productimage)models in one page in admin using tabular inline

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Product, Review,Brand,ProductImages
 # Register your models here.
 
-admin.site.register(Product)
+class ProductImageInline(admin.TabularInline):
+    model=ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'brand')
+    inlines=[ProductImageInline]
+
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Review)
 admin.site.register(Brand)
-admin.site.register(ProductImages)
