da54f258c90caddc41a59cacc30b2160b361d8c3
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-12-09 15:30:06 +0200

    add product list ,detail  using CBV

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product ,Brand , Review
+from django.views.generic import ListView ,DetailView
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
+
+
+class ProductDetail(DetailView):
+    model = Product
