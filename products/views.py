e50df42ef23cd6a138eece373e845be2a0f3cbb1
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2023-12-18 11:56:07 +0200

    add context of  prev & next in git context data inside view

--- products/views.py
+++ products/views.py
@@ -15,5 +15,29 @@ class ProductDetail(DetailView):
         context["reviews"] = Review.objects.filter(product =self.get_object()) #any single cbv (detail ,delete,update) have self.get_object()
         context['images'] = ProductImages.objects.filter(product = self.get_object())
         context['related'] =Product.objects.filter(brand =self.get_object().brand)
+
+        #activate next and prev product 
+        # Get the current product
+        current_product = self.get_object()
+
+        # Get the queryset of all products
+        all_products = Product.objects.all()
+
+        # Find the index of the current product in the queryset
+        current_index = list(all_products).index(current_product)
+
+        # Get the next product (if exists)
+        next_product = None
+        if current_index < len(all_products) - 1:
+            next_product = all_products[current_index + 1]
+
+        # Get the previous product (if exists)
+        prev_product = None
+        if current_index > 0:
+            prev_product = all_products[current_index - 1]
+
+        context['next_product'] = next_product
+        context['prev_product'] = prev_product
+    
         return context
     
