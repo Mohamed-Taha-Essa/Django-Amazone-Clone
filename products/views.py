fc8ffb8b3c454a7146cf44290aa96126fcff1e07
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2023-12-18 11:27:21 +0200

    add url to related item in product_detail.html

--- products/views.py
+++ products/views.py
@@ -11,8 +11,9 @@ class ProductDetail(DetailView):
     model = Product
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["reviews"] = Review.objects.filter(product =self.get_object())
+        context = super().get_context_data(**kwargs)  # dictionary of object from product
+        context["reviews"] = Review.objects.filter(product =self.get_object()) #any single cbv (detail ,delete,update) have self.get_object()
         context['images'] = ProductImages.objects.filter(product = self.get_object())
+        context['related'] =Product.objects.filter(brand =self.get_object().brand)
         return context
     
