4dcd9c014e7d00f9354eb37536eba232ea4b397e
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-12-20 18:14:15 +0200

    add api for product & brand list & detail

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from .views import ProductList ,ProductDetail ,BrandDetail,BrandList
+from . import api
+
 urlpatterns = [
     path("brands", BrandList.as_view()),
     path("brands/<slug:slug>", BrandDetail.as_view()),
     path('' ,ProductList.as_view() ),
-    path('<slug:slug>' ,ProductDetail.as_view() )
+    path('<slug:slug>' ,ProductDetail.as_view() ),
+
+
+    #api 
+    path('api/list' ,api.ProductListAPI.as_view() ),
+    path('api/list/<int:pk>' ,api.ProductDetailAPI.as_view() ),
+   
+    path('api/brand' ,api.BrandListAPI.as_view() ),
+    path('api/brand/<int:pk>' ,api.BrandDetailAPI.as_view() ),
+   
 ]
