ced20241e60f04adb69163879455d9cfa8847349
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-12-20 17:36:21 +0200

    add configuration of rest frame work

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #packages
     'taggit',
-
+    'rest_framework',
+    #apps
     'products',
 ]
 
