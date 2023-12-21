f03cdd1d2aa50b30b91b3ea48968c0bd328d2211
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2023-12-21 11:38:06 +0200

    add review detail in product detail api

--- products/serializers.py
+++ products/serializers.py
@@ -1,15 +1,22 @@
 #form 
 from rest_framework import serializers
-from .models import Product,Brand
+from .models import Product,Brand,Review ,ProductImages
+
+class ReviewSerializers(serializers.ModelSerializer):
+    class Meta:
+        model =Review 
+        fields = ['user' , 'content' ,'rate' ,'created_at']
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    
     class Meta:
         model = Product
         fields ='__all__'
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
+    review = ReviewSerializers(source ='review_product' ,many =True)
     class Meta:
         model = Product
         fields ='__all__'
