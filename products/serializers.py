4bd563303693d68c39bda06b27a739e7a9695318
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2023-12-23 10:32:26 +0200

    add avg rate in product detail

--- products/serializers.py
+++ products/serializers.py
@@ -30,16 +30,28 @@ class BrandListSerializer(serializers.ModelSerializer):
         fields ='__all__'
 
 class BrandDetailSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Brand
         fields ='__all__'
-
-
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     brand = BrandDetailSerializer()
     review = ReviewSerializers(source ='review_product' ,many =True)
     images = ProductImagesSerializers(source ='product_image' ,many = True)
+    avg_rate  =serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields ='__all__'
+
+    def get_avg_rate(self ,object):
+        reviews = object.review_product.all()
+        temp =0 
+        if len(reviews)>0:
+            for obj in reviews:
+                temp +=obj.rate
+            avg_rate = temp /len(reviews)
+        else :
+            avg_rate =0 
+        return avg_rate
