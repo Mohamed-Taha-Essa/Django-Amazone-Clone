f80057a75e283ac8ecb188f65c314b7e28cac600
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2023-12-23 10:48:30 +0200

    show avg rate in products API

--- products/models.py
+++ products/models.py
@@ -60,4 +60,4 @@ class Review(models.Model):
     created_at =models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.product
+        return self.user
--- products/serializers.py
+++ products/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from .models import Product,Brand,Review ,ProductImages
 
 class ReviewSerializers(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
     class Meta:
         model =Review 
         fields = ['user' , 'content' ,'rate' ,'created_at']
@@ -15,6 +16,8 @@ class ProductImagesSerializers(serializers.ModelSerializer):
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
     review_count =serializers.SerializerMethodField()
+    avg_rate  =serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields ='__all__'
@@ -22,6 +25,17 @@ class ProductListSerializer(serializers.ModelSerializer):
     def get_review_count(self,object ):
         reviews= object.review_product.all().count()
         return reviews
+    
+    def get_avg_rate(self ,object):
+        reviews = object.review_product.all()
+        temp =0 
+        if len(reviews)>0:
+            for obj in reviews:
+                temp +=obj.rate
+            avg_rate = temp /len(reviews)
+        else :
+            avg_rate =0 
+        return avg_rate
 
 
 class BrandListSerializer(serializers.ModelSerializer):
@@ -40,11 +54,17 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     review = ReviewSerializers(source ='review_product' ,many =True)
     images = ProductImagesSerializers(source ='product_image' ,many = True)
     avg_rate  =serializers.SerializerMethodField()
+    review_count =serializers.SerializerMethodField()
+
 
     class Meta:
         model = Product
         fields ='__all__'
 
+    def get_review_count(self,object ):
+        reviews= object.review_product.all().count()
+        return reviews
+    
     def get_avg_rate(self ,object):
         reviews = object.review_product.all()
         temp =0 
