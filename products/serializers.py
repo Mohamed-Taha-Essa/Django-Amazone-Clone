37f8a1166d2990799fb45615143f54389fde558a
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2023-12-23 10:23:29 +0200

    show number of review in every prodcut

--- products/serializers.py
+++ products/serializers.py
@@ -14,11 +14,14 @@ class ProductImagesSerializers(serializers.ModelSerializer):
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand = serializers.StringRelatedField()
-    
+    review_count =serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields ='__all__'
 
+    def get_review_count(self,object ):
+        reviews= object.review_product.all().count()
+        return reviews
 
 
 class BrandListSerializer(serializers.ModelSerializer):
---
+++ project/todo.md
@@ -0,0 +1,8 @@
+# in API:
+- api list & detail for product and brand.
+- add pagination using CBV.
+- add custom pagination.
+- show name of brand in product list using  serializers.StringRelatedField() 
+- show brand ,images and review detail in product detail api .
+- show review number on product.
+- show avg of review on product.
