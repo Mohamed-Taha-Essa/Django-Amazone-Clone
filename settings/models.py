2c660bb96f4805c88a6ba978f7f41fe7451ff2b4
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-01-05 23:19:34 +0200

    build model setting

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Settings(models.Model):
+    name = models.CharField( max_length=50)
+    logo = models.ImageField(upload_to='settings')
+    subtitle = models.TextField(max_length=500 )
+    call_us = models.CharField( max_length=50)
+    email_us = models.CharField( max_length=50)
+
+    emails = models.TextField(max_length = 50)
+    phones = models.TextField(max_length = 50)
+    address = models.TextField(max_length = 10)
+
+    android_app = models.URLField(null=True,blank=True)
+    ios_app = models.URLField(null=True,blank=True)
+    facebook = models.URLField(null=True,blank=True)
+    youtube = models.URLField(null=True,blank=True)
+    twitter = models.URLField(null=True,blank=True)
