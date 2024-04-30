af2a08b93381d6c59ad4a660ed28fbbec001b76a
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-04-30 22:23:21 +0300

    add static files root

--- project/settings.py
+++ project/settings.py
@@ -185,7 +185,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =   BASE_DIR / "static",
+STATIC_ROOT =   BASE_DIR / "staticfiles",
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
