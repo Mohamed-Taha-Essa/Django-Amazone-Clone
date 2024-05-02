084f8e4e62ba63d6c842439e21d84b0ac07a31c4
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-05-02 23:43:22 +0300

    update top_order,top_rate and top disc in home

--- project/settings.py
+++ project/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'mathfilters',
     #apps
     'products',
     'settings',
--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.db.models import Count
-from products.models import Product ,Review,Brand
+from django.db.models import Count ,Sum
+from products.models import Product ,Review,Brand ,OrderProduct
+from orders.models import OrderDetail
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def multiply(value, arg):
+    return value * arg
 
 # Create your views here.
 def home(request):
@@ -15,10 +24,37 @@ def home(request):
     reviews = Review.objects.all()[:10]
 
    
+    # order_details = OrderDetail.objects.all()
+    top_order_products = (
+        OrderDetail.objects
+        .values('product__id', 'product__name','product__image','product__price' ,'product__slug','product__quantity')  # Group by Product ID and name
+        .annotate(total_quantity=Sum('quantity'))  # Calculate total quantity per Product
+        .order_by('-total_quantity')  # Order by total quantity in descending order
+    )
+    products_list =[]
+    for item in top_order_products:
+        product_data = {
+            'id': item['product__id'],
+            'name': item['product__name'],
+            'image': item['product__image'],
+            'quantity': item['product__quantity'],
+
+            'price': item['product__price'],
+            'slug': item['product__slug'],
+            'review_count': Product.objects.get(id=item['product__id']).review_count,
+            'avg_rate': Product.objects.get(id=item['product__id']).avg_rate,
+        }
+        products_list.append(product_data)
+        
+    avg_rate = sorted(products_list,key=lambda x: x['avg_rate'],reverse=True)
+    top_disc = sorted(products_list,key=lambda x: x['avg_rate'])
+
     return render(request ,'settings/home.html',{
         'new_product':new_product,
         'sale_product':sale_product,
         'feature_product':feature_product,
         'brands':brands,
-        'reviews':reviews,
+        'top_order_products':products_list,
+        'top_rate':avg_rate,
+        'top_disc':top_disc,
     })
